33bc1516156a790a2ca65685d19bb00f19af26d6
khushalhiranii <khushal.hirani4@gmail.com>
2024-10-17 02:43:13 +0530

    logo rotated

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="https://gallerypng.com/wp-content/uploads/2024/07/shinchan-png-photo.png" className='app-logo' />
+        <img src="https://gallerypng.com/wp-content/uploads/2024/07/shinchan-png-photo.png" loading='lazy' className="App-logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
