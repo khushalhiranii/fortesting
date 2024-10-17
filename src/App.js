f1d023b5e4b68d778cbb28779f621216caaf7b41
khushalhiranii <khushal.hirani4@gmail.com>
2024-10-17 17:06:40 +0530

    alt removed

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fgallerypng.com%2Fshinchan-png-photo-free-hd%2F&psig=AOvVaw2kww0WJSD1wvL1hv_5PDvT&ust=1729251162791000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCJDYrNyolYkDFQAAAAAdAAAAABAE" className="App-logo" alt="logo" />
+        <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fgallerypng.com%2Fshinchan-png-photo-free-hd%2F&psig=AOvVaw2kww0WJSD1wvL1hv_5PDvT&ust=1729251162791000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCJDYrNyolYkDFQAAAAAdAAAAABAE" className="App-logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
