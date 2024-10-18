dc811b1a91f890e1b2ffd2cd2333d6d730c5d5d2
khushalhiranii <khushal.hirani4@gmail.com>
2024-10-18 17:26:15 +0530

    app multipage

--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
-// import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Dashboard from './pages/Dashboard';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="https://gallerypng.com/wp-content/uploads/2024/07/shinchan-png-photo.png" className="App-logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <Login/>
-    </div>
+    <Router basename="/">
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/" element={<Login />} /> {/* Default route */}
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
