ab2dd9ad37b7eb1d7fb429a67d85fdc2000bced0
khushalhiranii <khushal.hirani4@gmail.com>
2024-10-21 14:38:07 +0530

    package sorted

--- craco.config.js
+++ craco.config.js
@@ -1,13 +1,23 @@
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+
 module.exports = {
   webpack: {
-    configure: (webpackConfig) => {
+    configure: (webpackConfig, { env }) => {
+      // Remove React Refresh plugin for production builds
+      if (env === 'production') {
+        webpackConfig.plugins = webpackConfig.plugins.filter(
+          (plugin) => !(plugin instanceof ReactRefreshWebpackPlugin)
+        );
+      }
+
       webpackConfig.output = {
         ...webpackConfig.output,
-        filename: 'lms-widget.js',  // Bundle into a single JS file
-        library: 'LMSWidget',       // Expose it as a global variable
-        libraryTarget: 'umd',       // Use UMD for compatibility
-        globalObject: 'this',       // Ensure compatibility across environments
+        filename: 'lms-widget.js',  // Widget file
+        library: 'LMSWidget',       // Expose as global
+        libraryTarget: 'umd',       // UMD format for compatibility
+        globalObject: 'this',       // Ensure global compatibility
       };
+
       return webpackConfig;
     },
   },
